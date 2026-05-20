71f6d6fd9e611d069a4532da2bae8dec2da4b537
HackRogue <mazumdarh549@gmail.com>
2026-05-20 22:33:21 +0530

    Fixed changes

--- Admin.js
+++ Admin.js
@@ -1,2 +1,14 @@
 import {Kafka} from "kafkajs";
 
+const kafka = new Kafka({
+    clientId = 'my-app',
+    brokers = ['http://localhost:9092']
+})
+
+async function init() {
+    const admin = kafka.admin;
+    console.log('Admin Connecting...');
+    admin.connect();
+    console.log('Admin connected successfully');
+
+}
