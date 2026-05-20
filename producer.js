5696e9df71d3c4d7241efcd25257594f227770fc
HackRogue <mazumdarh549@gmail.com>
2026-05-20 23:11:17 +0530

    added a sample producer code

--- producer.js
+++ producer.js
@@ -1,24 +1,35 @@
 import {kafka} from "./client.js"
 
-const init = () => {
+const init = async () => {
     const producer = kafka.producer()
 
     await producer.connect()
-    console.log("Producer connected Successfully")
+    console.log("Producer connected Successfully...")
 
     await producer.send({
         topic: 'rider-updates',
         messages: [
-            { key: 'location-updates', value: JSON.stringify({ 
-                rider : "Amrit Mishra",
-                time : "20:00:07", 
-                coordinates : "[19.61, 78.67]" })
+            { key: 'location-updates', 
+                partition : 0, 
+                value: JSON.stringify({ 
+                    rider : "Amrit Mishra",
+                    time : "20:00:07", 
+                    coordinates : "[19.61, 78.67]"
+                 })
             },
-            { key: 'location-updates', value: JSON.stringify({ 
-                rider : "Ram Sharma",
-                time : "20:00:07", 
-                coordinates : "[25.78, 73.69]" })
+            { key: 'location-updates', 
+                partition : 0, 
+                value: JSON.stringify({ 
+                    rider : "Ram Sharma",
+                    time : "20:00:07", 
+                    coordinates : "[25.78, 73.69]" 
+                })
             },
          ],
     })
+
+    await producer.disconnect()
+    console.log('Producer disconnected successfully...')
 }
+
+init()
