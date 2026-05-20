ee3b768f00d3db480a57d147736ca38c66b2e945
HackRogue <mazumdarh549@gmail.com>
2026-05-20 23:33:08 +0530

    The single prod-consumer aplication for rider updates is ready

--- consumer.js
+++ consumer.js
@@ -1,2 +1,19 @@
 import { kafka } from "./client.js"
 
+async function init(){
+    const consumer = kafka.consumer({
+        groupId : "group-1"
+    })
+    console.log("Consumer connected successfully...")
+    await consumer.connect()
+
+    await consumer.subscribe({ topics: ['rider-updates'], fromBeginning : true })
+
+    await consumer.run({
+        eachMessage : async ({topic, partition, message, heartbeat, pause}) => {
+            console.log(`TOPIC : ${topic} PARTITION : ${partition}`, message.value.toString())
+        }
+    })
+}
+
+init()
