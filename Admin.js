921d651a07c193d4e5ef26d02bb1fa955d194ce8
HackRogue <mazumdarh549@gmail.com>
2026-05-20 22:37:41 +0530

    topic creation checkpoint

--- Admin.js
+++ Admin.js
@@ -11,4 +11,12 @@ async function init() {
     admin.connect();
     console.log('Admin connected successfully');
 
+    console.log("Creating Topic rider-updates")
+    await admin.createTopics({
+        topics: [{
+            'topic' : 'rider-updates',
+            numPartitions : 2
+        }]
+    })
+
 }
