a189c2565649f91ac90e630f27ffc1d5b01f261c
HackRogue <mazumdarh549@gmail.com>
2026-05-20 23:07:21 +0530

    added a sample producer code

---
+++ producer.js
@@ -0,0 +1,24 @@
+import {kafka} from "./client.js"
+
+const init = () => {
+    const producer = kafka.producer()
+
+    await producer.connect()
+    console.log("Producer connected Successfully")
+
+    await producer.send({
+        topic: 'rider-updates',
+        messages: [
+            { key: 'location-updates', value: JSON.stringify({ 
+                rider : "Amrit Mishra",
+                time : "20:00:07", 
+                coordinates : "[19.61, 78.67]" })
+            },
+            { key: 'location-updates', value: JSON.stringify({ 
+                rider : "Ram Sharma",
+                time : "20:00:07", 
+                coordinates : "[25.78, 73.69]" })
+            },
+         ],
+    })
+}
